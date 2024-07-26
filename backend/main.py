9dcf902da20037fd813807cacac5e58fd5dc97c9
Alexis117 <alexispina117@gmail.com>
2024-07-25 18:23:31 -0600

    Adding state validation

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,6 @@
+import secrets
+import uuid
+
 from fastapi import FastAPI, Response, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse, JSONResponse
@@ -26,21 +29,30 @@ app.add_middleware(
 
 @app.get("/priv/home")
 async def home(request: Request):
-    return request.user # This will return the user object from the middleware
+    return request.user  # This will return the user object from the middleware
 
 
 @app.get("/auth/login")
 async def login():
+    state = secrets.token_urlsafe(16)
     auth_url = keycloak_openid.auth_url(
         redirect_uri=OPEN_ID_CALLBACK_URL,
         scope="openid",
-        state="12345"
+        state=state  # This is used to prevent CSRF attacks
     )
-    return RedirectResponse(auth_url)
+    response = RedirectResponse(auth_url)
+    session_state_x = CookieEncrypter.encrypt(state)
+    response.set_cookie(key="session-state-x",
+                        value=session_state_x, httponly=True)
+    return response
 
 
 @app.get("/auth/callback")
-async def callback(code: str, state: str, response: Response):
+async def callback(code: str, state: str, request: Request, response: Response):
+    session_state_x = request.cookies.get("session-state-x")
+    state_from_session = CookieEncrypter.decrypt(session_state_x)
+    if state != state_from_session:  # Validate the session comes from the same user
+        return JSONResponse(status_code=401, content={"error": "Authentication error", "message": "Invalid state"})
     try:
         token = keycloak_openid.token(
             grant_type='authorization_code',
@@ -48,15 +60,16 @@ async def callback(code: str, state: str, response: Response):
             redirect_uri=OPEN_ID_CALLBACK_URL,
         )
         keycloak_openid.userinfo(token['access_token'])
-    except KeycloakOperationError as e:       
-        return {"error": "Authentication error", "status": e.response_code, "message": e.error_message}
+    except KeycloakOperationError as e:
+        return JSONResponse(status_code=401, content={"error": "Authentication error", "message": e.error_message})
     except Exception as e:
         return RedirectResponse(url="/auth/login")
     response = RedirectResponse(url=FRONTEND_HOME_URL)
     session_x = CookieEncrypter.encrypt(token['access_token'])
     session_refresh_x = CookieEncrypter.encrypt(token['refresh_token'])
-    response.set_cookie(key="session-x" , value=session_x, httponly=True)
-    response.set_cookie(key="session-refresh-x", value=session_refresh_x, httponly=True)
+    response.set_cookie(key="session-x", value=session_x, httponly=True)
+    response.set_cookie(key="session-refresh-x",
+                        value=session_refresh_x, httponly=True)
     return response
 
 
